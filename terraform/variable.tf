e7e097dbc37806fb93a33f9f60d0c9aafc0d394b
dbramanayanam <152831955+dbramanayanam@users.noreply.github.com>
2024-11-19 14:19:01 +0400

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,41 @@
+variable "common_tags" {
+  #type        = map
+  default     = {
+     project = "roboshop"
+     environment = "dev"
+     terraform = "yes"
+
+  }
+}
+
+variable "vpc_tags" {
+  default     = {}
+  
+}
+
+variable "ec2_tags" {
+  default     = {}
+  
+}
+
+
+
+variable "project" {
+  type        = string
+  default     = "roboshop"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.small"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "dineshdevops.online"
+}
