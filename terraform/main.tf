4c44e27b846e92de2f8be2f6b30bac392e7bb749
dbramanayanam <152831955+dbramanayanam@users.noreply.github.com>
2024-11-23 17:02:05 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,21 @@ module "catalogue" {
   )
 }
 
+module "ansible" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.devops-practice.id
+  name                   = "${local.ec2_name}-ansible"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.openvpn_sg_id.value]
+  subnet_id              = data.aws_subnet.selected.id  #  default VPC 1a subnet
+  user_data = file("ec2-provision.sh")
+  tags = merge(
+    var.common_tags,
+    {
+      Component = "ansible"
+    },
+    {
+      Name = "${local.ec2_name}-ansible"
+    }
+  )
+}
